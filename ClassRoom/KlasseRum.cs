21ae3e7cdcf86d5985128e7403d283544d570594
Hassan Ali Mohdi <hm98-@hotmail.dk>
2018-01-29 12:07:07 +0100

    opgave 2

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        
+        private string _klasseNavn;
+        private DateTime _semesterStart;
+        private List<Studerende> _klasseListe;
+
+
+
+
+
+        public KlasseRum()
+        {
+            
+        }
+
+
+
+    }
+}

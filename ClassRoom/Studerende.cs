86297e4eae7d01419348b95d120823de9d123c2c
Hassan Ali Mohdi <hm98-@hotmail.dk>
2018-01-29 10:57:21 +0100

    Opgave 1

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        private string _navn;
+        private int _fødselsmåned;
+        private int _fødselsdag;
+
+        public Studerende(string navn, int fødselsmåned, int fødselsdag)
+        {
+            _navn = navn;
+            _fødselsmåned = fødselsmåned;
+            _fødselsdag = fødselsdag;
+
+
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { value = _navn; }
+
+        }
+
+        public int Fødselsmåned
+        {
+            get { return _fødselsmåned; }
+            set { value = _fødselsmåned; }
+        }
+
+
+        public int Fødselsdag
+        {
+            get { return _fødselsdag; }
+            set { value = _fødselsdag; }
+        }
+
+    
+
+
+
+
+    }
+}

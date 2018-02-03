38f821c1e7da76f0b30f7efc855772f3067f4d0a
Hassan Ali Mohdi <hm98-@hotmail.dk>
2018-02-03 08:11:10 +0100

    Ekstra opgave done

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +15,9 @@ namespace ClassRoom
 
             KlasseRum kr= new KlasseRum();
         
-            Studerende hassan= new Studerende("Hassan", 07, 09);
-            Studerende nikolaj = new Studerende("Nikolaj", 02, 01);
-            Studerende kasper = new Studerende("Kasper", 01, 02);
+            Studerende hassan= new Studerende("Hassan", 7, 09);
+            Studerende nikolaj = new Studerende("Nikolaj", 2, 01);
+            Studerende kasper = new Studerende("Kasper", 1, 02);
         
             kr._klasseListe.Add(hassan);
             kr.KlasseListe.Add(nikolaj);
@@ -33,26 +34,30 @@ namespace ClassRoom
 
 
             var vinter = (from Studerende in kr._klasseListe
-                where Studerende.Fødselsmåned == 01||Studerende.Fødselsmåned==02||Studerende.Fødselsmåned==03
+                where Studerende.Fødselsmåned == 1||Studerende.Fødselsmåned==2||Studerende.Fødselsmåned==3
                 select Studerende.Navn).Count();
 
             var forår = (from Studerende in kr._klasseListe
-                where Studerende.Fødselsmåned == 04 || Studerende.Fødselsmåned == 05 || Studerende.Fødselsmåned == 06
+                where Studerende.Fødselsmåned == 4 || Studerende.Fødselsmåned == 5 || Studerende.Fødselsmåned == 6
                 select Studerende.Navn).Count();
 
             var sommer = (from Studerende in kr._klasseListe
-                where Studerende.Fødselsmåned == 07 || Studerende.Fødselsmåned == 08 || Studerende.Fødselsmåned == 09
+                where Studerende.Fødselsmåned == 7 || Studerende.Fødselsmåned == 8 || Studerende.Fødselsmåned == 9
                 select Studerende.Navn).Count();
 
             var efterår = (from Studerende in kr._klasseListe
-                where Studerende.Fødselsmåned == 04 || Studerende.Fødselsmåned == 05 || Studerende.Fødselsmåned == 06
+                where Studerende.Fødselsmåned == 4 || Studerende.Fødselsmåned == 5 || Studerende.Fødselsmåned == 6
+                select Studerende.Navn).Count();
+
+            var forkertmåned = (from Studerende in kr._klasseListe
+                where Studerende.Fødselsmåned <0 || Studerende.Fødselsmåned >12
                 select Studerende.Navn).Count();
 
             Console.WriteLine(vinter+" har fødseldag i vinter");
             Console.WriteLine(forår + " har fødseldag i forår");
             Console.WriteLine(sommer + " har fødseldag i sommer");
             Console.WriteLine(efterår + " har fødseldag i efterår");
-
+            Console.WriteLine(forkertmåned + " studerende har valg en forket måned ude for intervallet 1-12");
 
             Console.ReadKey();
 
